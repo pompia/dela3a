ccf29307d6864244bf69729c66d99753c7510e16
pompia <45210867+pompia@users.noreply.github.com>
2018-12-01 23:02:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,24 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+client.on('message', message => {
+        var prefix = "+";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
 
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+
+
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+
+
+      });
 client.on('ready', () => {
    console.log(`----------------`);
    console.log(`Credit Farmm - Script By : iiAKRAM_DZ `);
